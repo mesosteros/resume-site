89e2a7ea8884aa870faab28a16c4312ea5392dcb
Carlos Santos <mesosteros@users.noreply.github.com>
2019-06-30 23:49:28 +0100

    [6] Add animation to toggle navbar menu item "resume"

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+    group
+} from '@angular/animations';
 
 @Component({
     selector: 'app-nav-bar',
     templateUrl: './nav-bar.component.html',
     styleUrls: ['./nav-bar.component.scss'],
+    animations: [
+        trigger('openClose', [
+            state('in', style({ height: '*', opacity: 0 })),
+            transition(':leave', [
+                style({ height: '*', opacity: 1 }),
+                group([
+                    animate(200, style({ height: 0 })),
+                    animate('200ms ease-in-out', style({ opacity: 0 }))
+                ])
+            ]),
+            transition(':enter', [
+                style({ height: '0', opacity: 0 }),
+                group([
+                    animate(300, style({ height: '*' })),
+                    animate('300ms ease-in-out', style({ opacity: 1 }))
+                ])
+
+            ])
+        ])
+    ]
 })
 export class NavBarComponent implements OnInit {
     resumeSectionExpanded = false;
