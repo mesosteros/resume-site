932e905ebc2cc4ae2593bb7d35f88fac1b8e3efc
Carlos Santos <mesosteros@users.noreply.github.com>
2019-07-02 23:47:57 +0100

    Add font awesome icons to menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// tslint:disable-next-line:max-line-length
+import { faBusinessTime, faCaretDown, faCaretUp, faLaptopCode, faUserCircle, faUserGraduate, faWrench } from '@fortawesome/pro-regular-svg-icons';
+import { faDiceD20, faFileCertificate } from '@fortawesome/pro-light-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +14,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 
+/**
+ * App Base Module
+ *
+ * @export
+ * @class AppModule
+ */
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,4 +36,20 @@ import { ContentComponent } from './content/content.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  /**
+   * Creates an instance of AppModule.
+   * @memberof AppModule
+   */
+  constructor() {
+    library.add(faBusinessTime);
+    library.add(faCaretDown);
+    library.add(faCaretUp);
+    library.add(faLaptopCode);
+    library.add(faUserCircle);
+    library.add(faUserGraduate);
+    library.add(faWrench);
+    library.add(faDiceD20);
+    library.add(faFileCertificate);
+  }
+}
