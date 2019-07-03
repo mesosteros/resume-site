140d89de994022840fe46d0eba7dcf8fe4f8c20c
Carlos Santos <mesosteros@users.noreply.github.com>
2019-07-03 01:16:58 +0100

    Add missing documentation to content component

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * Content Component
+ *
+ * @export
+ * @class ContentComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
-
+  /**
+   * Creates an instance of ContentComponent.
+   * 
+   * @memberof ContentComponent
+   */
   constructor() { }
 
+  /**
+   * Lifecycle hook
+   *
+   * @memberof ContentComponent
+   */
   ngOnInit() {
   }
 
