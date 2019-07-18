741fc1b5c9793f43955dd5e5b3102c3d9b6b8bb9
Carlos Santos <mesosteros@users.noreply.github.com>
2019-07-19 00:47:15 +0100

    Add skill list

--- src/app/resume/skills/skills.component.ts
+++ src/app/resume/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, zip, of } from 'rxjs';
 
 /**
  * Skills Component
@@ -13,17 +14,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  public skillsList: Array<any>;
   /**
    * Creates an instance of SkillsComponent.
    *
    * @memberof SkillsComponent
    */
-  constructor() {}
+  constructor() {
+    const baseArray = new Array();
+    this.skillsList = [...baseArray, ...baseArray];
+  }
 
   /**
    * Lifecycle hook
    *
    * @memberof SkillsComponent
    */
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateSkillsList().subscribe(
+      (skills: Array<any>): void => {
+        this.skillsList = skills;
+      }
+    );
+  }
+
+  private generateSkillsList(): Observable<Array<any>> {
+    const mainSkills = this.generateMainSkills();
+    const secondarySkills = this.generateSecondarySkills();
+    return of(new Array(mainSkills, secondarySkills));
+  }
+
+  private generateMainSkills(): Array<any> {
+    return [
+      { skillId: 'JavaScript/TypeScript', skillImage: '' },
+      { skillId: 'Angular', skillImage: '' },
+      { skillId: 'Git', skillImage: '' },
+      { skillId: 'Jasmine', skillImage: '' },
+      { skillId: 'Jest', skillImage: '' },
+      { skillId: 'HTML/CSS', skillImage: '' }
+    ];
+  }
+
+  private generateSecondarySkills(): Array<any> {
+    return [
+      { skillId: 'Scrum' },
+      { skillId: 'MeteorJS' },
+      { skillId: 'NestJs' },
+      { skillId: 'angularJs' },
+      { skillId: 'bootstrap' },
+      { skillId: 'bulma' },
+      { skillId: 'SCSS' },
+      { skillId: 'MongoDB' }
+    ];
+  }
 }
