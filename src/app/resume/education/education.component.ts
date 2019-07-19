c3dc552040340dfc38e8411e8933dfbadb6970f8
Carlos Santos <mesosteros@users.noreply.github.com>
2019-07-19 01:49:03 +0100

    Add basic education data and structure

--- src/app/resume/education/education.component.ts
+++ src/app/resume/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 /**
  * Education Component
@@ -13,6 +14,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent implements OnInit {
+  public educationHistory: Array<any>;
+  public months = [
+    'Jan.',
+    'Feb.',
+    'Mar.',
+    'Apr.',
+    'May',
+    'June',
+    'July',
+    'Aug.',
+    'Sep.',
+    'Oct.',
+    'Nov.',
+    'Dec.'
+  ];
+
   /**
    * Creates an instance of EducationComponent.
    *
@@ -25,5 +42,73 @@ export class EducationComponent implements OnInit {
    *
    * @memberof EducationComponent
    */
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateEducationList().subscribe(
+      (educationData: Array<any>): void => {
+        this.educationHistory = educationData;
+      }
+    );
+  }
+
+  /**
+   * Returns an observable stream of all education data
+   *
+   * @private
+   * @returns {Observable<Array<any>>}
+   * @memberof EducationComponent
+   */
+  private generateEducationList(): Observable<Array<any>> {
+    return of(this.generateEducationHistory());
+  }
+
+  /**
+   * Generates Education Data Array
+   *
+   * @private
+   * @returns {Array<any>}
+   * @memberof EducationComponent
+   */
+  private generateEducationHistory(): Array<any> {
+    return [
+      {
+        id: 'University of Trás-os-Montes e Alto Douro',
+        image: 'https://bulma.io/images/placeholders/128x128.png',
+        shortDescription:
+          'Bachelor of Information and Communication Technology',
+        longDescription: ['Information Systems', 'Programming Methodologies'],
+        location: 'Vila Real, Portugal',
+        type: 'Bachelor of Information and Communication Technology',
+        startDate: this.formatDateForCV(9, 2006),
+        endDate: this.formatDateForCV(7, 2010)
+      },
+      {
+        id: 'University of Linköping',
+        image: 'https://bulma.io/images/placeholders/128x128.png',
+        shortDescription: 'Erasmus',
+        longDescription: ['Erasmus experience for 1 semester'],
+        location: 'Linköping, Sweden',
+        type: 'Junior Consultant',
+        startDate: this.formatDateForCV(8, 2008),
+        endDate: this.formatDateForCV(1, 2009)
+      }
+    ];
+  }
+
+  /**
+   * Formats the date to be CV conforming
+   *
+   * @private
+   * @param {number} month
+   * @param {number} year
+   * @returns {*}
+   * @memberof EducationComponent
+   */
+  private formatDateForCV(month: number, year: number): any {
+    const d = new Date(year, month - 1);
+    const formattedDateString = `${
+      this.months[d.getMonth()]
+    }, ${d.getFullYear()}`;
+
+    return formattedDateString;
+  }
 }
