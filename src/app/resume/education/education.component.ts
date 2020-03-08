d1c46c6b9e69460f96fecd19802c20b34529a0cd
Carlos Santos <mesosteros@users.noreply.github.com>
2020-03-08 19:22:10 +0000

    Education component

--- src/app/resume/education/education.component.ts
+++ src/app/resume/education/education.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Observable, of } from "rxjs";
 
 /**
  * Education Component
@@ -9,25 +9,25 @@ import { Observable, of } from 'rxjs';
  * @implements {OnInit}
  */
 @Component({
-  selector: 'app-education',
-  templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  selector: "app-education",
+  templateUrl: "./education.component.html",
+  styleUrls: ["./education.component.scss"]
 })
 export class EducationComponent implements OnInit {
   public educationHistory: Array<any>;
   public months = [
-    'Jan.',
-    'Feb.',
-    'Mar.',
-    'Apr.',
-    'May',
-    'June',
-    'July',
-    'Aug.',
-    'Sep.',
-    'Oct.',
-    'Nov.',
-    'Dec.'
+    "Jan.",
+    "Feb.",
+    "Mar.",
+    "Apr.",
+    "May",
+    "June",
+    "July",
+    "Aug.",
+    "Sep.",
+    "Oct.",
+    "Nov.",
+    "Dec."
   ];
 
   /**
@@ -71,23 +71,25 @@ export class EducationComponent implements OnInit {
   private generateEducationHistory(): Array<any> {
     return [
       {
-        id: 'University of Trás-os-Montes e Alto Douro',
-        image: 'https://bulma.io/images/placeholders/128x128.png',
+        id: "University of Trás-os-Montes e Alto Douro",
+        image:
+          "https://media1-production-mightynetworks.imgix.net/asset/4592423/UTAD-round.jpg?ixlib=rails-0.3.0&fm=jpg&q=100&auto=format&w=256&h=256&fit=crop&crop=faces",
         shortDescription:
-          'Bachelor of Information and Communication Technology',
-        longDescription: ['Information Systems', 'Programming Methodologies'],
-        location: 'Vila Real, Portugal',
-        type: 'Bachelor of Information and Communication Technology',
+          "Bachelor of Information and Communication Technology",
+        longDescription: ["Information Systems", "Programming Methodologies"],
+        location: "Vila Real, Portugal",
+        type: "Bachelor of Information and Communication Technology",
         startDate: this.formatDateForCV(9, 2006),
         endDate: this.formatDateForCV(7, 2010)
       },
       {
-        id: 'University of Linköping',
-        image: 'https://bulma.io/images/placeholders/128x128.png',
-        shortDescription: 'Erasmus',
-        longDescription: ['Erasmus experience for 1 semester'],
-        location: 'Linköping, Sweden',
-        type: 'Junior Consultant',
+        id: "University of Linköping",
+        image:
+          "https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1409787711/tzayonkjejyttfn4sson.jpg",
+        shortDescription: "Erasmus",
+        longDescription: ["Erasmus experience for 1 semester"],
+        location: "Linköping, Sweden",
+        type: "Junior Consultant",
         startDate: this.formatDateForCV(8, 2008),
         endDate: this.formatDateForCV(1, 2009)
       }
