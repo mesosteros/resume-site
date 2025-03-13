7d634623b181067ada50de0941a1e2f1df197f75
Carlos Santos <108001241+carlosersantos@users.noreply.github.com>
2025-03-13 13:46:09 +0000

    Remove hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideClientHydration(withEventReplay()),
   ],
 };
